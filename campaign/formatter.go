a57a9a43bd185e4616f9944606ef60af79bc68cb
Reza Rizqi Ramdhani <rezarizqir@gmail.com>
2022-05-27 15:55:46 +0700

    Update: Campaign formatter

--- campaign/formatter.go
+++ campaign/formatter.go
@@ -1,1 +1,37 @@
 package campaign
+
+type CampaignFormatter struct {
+	ID               int    `json:"id"`
+	Name             string `json:"name"`
+	ShortDescription string `json:"short_description"`
+	ImageUrl         string `json:"image_url"`
+	GoalAmount       int    `json:"goal_amount"`
+	CurrentAmount    int    `json:"current_amount"`
+	UserId           int    `json:"user_id"`
+}
+
+func FormatCampaign(campaign Campaign) CampaignFormatter {
+	campaignFormatter := CampaignFormatter{}
+	campaignFormatter.Name = campaign.Name
+	campaignFormatter.ShortDescription = campaign.ShortDescription
+	campaignFormatter.ID = campaign.ID
+	campaignFormatter.CurrentAmount = campaign.CurrentAmount
+	campaignFormatter.GoalAmount = campaign.GoalAmount
+	campaignFormatter.UserId = campaign.UserId
+
+	if len(campaign.CampaignImages) > 0 {
+		campaignFormatter.ImageUrl = campaign.CampaignImages[0].FileName
+	}
+
+	return campaignFormatter
+}
+
+func FormatCampaigns(campaigns []Campaign) []CampaignFormatter {
+	campaignFormatter := []CampaignFormatter{}
+
+	for _, campaign := range campaigns {
+		campaignFormatter = append(campaignFormatter, FormatCampaign(campaign))
+	}
+
+	return campaignFormatter
+}
--- handler/campaign.go
+++ handler/campaign.go
@@ -30,6 +30,6 @@ func (h *campaignHandler) GetCampaigns(c *gin.Context) {
 		return
 	}
 
-	response := helper.APIResponse("Get campaign success", http.StatusOK, "success", campaigns)
+	response := helper.APIResponse("Get campaign success", http.StatusOK, "success", campaign.FormatCampaigns(campaigns))
 	c.JSON(http.StatusOK, response)
 }
